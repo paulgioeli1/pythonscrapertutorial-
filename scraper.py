98bd4b026c2922939200fb3d39a6386e43f6ebbb
paulgioeli1 <104484028+paulgioeli1@users.noreply.github.com>
2022-05-02 21:57:21 -0700

    Update scraper.py
    additional work

--- scraper.py
+++ scraper.py
@@ -1,1 +1,18 @@
-print("Hello World")
+#First Python Scraper
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://realpython.github.io/fake-jobs/"
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(id = "ResultsContainer")
+#print(results.prettify())
+
+job_elements = soup.find_all("div", class_ = "card-content" )
+#print(job_elements)
+
+for job in job_elements:
+    print(job, end = "\n"*2)
